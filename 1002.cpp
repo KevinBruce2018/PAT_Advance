0265b42e22f93747d372ade49102fe5d1dac2416
KevinBruce2018 <45493765+KevinBruce2018@users.noreply.github.com>
2019-09-10 19:00:45 +0800

    修改成了正确的代码

--- 1002.cpp
+++ 1002.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    //WA  哭了  下次接着调整
-    int max=0;
-    double polynomial1[1001]={0},polynomial2[1001]={0};
+    int count=0;
+    double polynomial[1001]={0};
     int n;
     cin>>n;
     for(int i = 0;i < n;i++)
@@ -12,9 +11,7 @@ int main(int argc, char const *argv[])
         int a;
         double b;
         cin>>a>>b;
-        if(a>max)
-            max = a;
-        polynomial1[a] = b;
+        polynomial[a] = b;
     }
     cin>>n;
     for(int i = 0;i < n;i++)
@@ -22,14 +19,17 @@ int main(int argc, char const *argv[])
         int a;
         double b;
         cin>>a>>b;
-        if(a>max)
-            max = a;
-        polynomial2[a] = b;
+        polynomial[a] += b;
     }
-    cout<<max+1;
-    for(int i = max;i>=0;i--)
-    {
-        printf(" %d %.1lf",i,polynomial1[i]+polynomial2[i]);
+    //0项不输出，但是题目没说，自己WA以后猜的
+    for(int i = 0;i <= 1000;i++)
+        if(polynomial[i])
+            count++;
+    cout<<count;
+    
+    for(int i = 1000;i>=0;i--){
+        if(polynomial[i])
+            printf(" %d %.1lf",i,polynomial[i]);
     }
     cout<<endl;
     return 0;
